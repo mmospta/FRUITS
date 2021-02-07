e17abe296b6ed29969c39715eda5d861d3b17014
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scb.co.th>
2021-02-07 15:59:45 +0700

    Initial Project Fructus

---
+++ Fructus/Fructus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Phonthep Aungkanukulwit on 7/2/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Phonthep Aungkanukulwit on 7/2/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
